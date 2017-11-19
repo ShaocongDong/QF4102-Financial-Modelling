00d47876fd46cc9f66a4bd66243569c091b9a54f
Dong Shaocong <shaocong.dong@yahoo.com>
2017-11-19 13:56:23 +0800

    The monotonicity conditions are satisfied.

--- Assignment3/HelperScript.m
+++ Assignment3/HelperScript.m
@@ -6,16 +6,15 @@ r = 0.03;
 xmax = 5;
 N = 1500;
 T = 1;
-I = 4;
+I = 100;
 
-dt = T/N;
-dx = xmax/I; % as defined in the question
+for I = 100:100:1500
+    dt = T/N;
+    dx = xmax/I; % as defined in the question
 
-alpha = sigma^2*dt/(dx^2);
-beta = dt*(r-q-sigma^2/2)/(2*dx);
-a = (beta - alpha/2) * ones(2*I-1,1)*100000;
-b = (1 + alpha + r*dt) * ones(2*I-1,1)*100000;
-c = (-alpha/2 - beta) * ones(2*I-1,1)*100000;
-disp([alpha, beta]);
-disp([ a,b,c]);
-CoeffMatrix=spdiags ([c, b, a],-1:1, 2*I-1, 2*I-1)';
+    alpha = sigma^2*dt/(dx^2);
+    beta = dt*(r-q-sigma^2/2)/(2*dx);
+    c = (-alpha/2 - beta);
+    disp([alpha, beta]);
+    disp(c);
+end
